64ab6e604e7a85f11c5ad38bdf0643e6b5b5ad76
Léni Gauffier <leni.gauffier@obspm.fr>
2021-03-03 18:50:30 +0000

    [Examples] Declination with sexag obliquity

--- examples/tests/test_notebooks.py
+++ examples/tests/test_notebooks.py
@@ -22,11 +22,13 @@ class TestNotebooks:
         assert data == (Sexagesimal(result) * degree)._repr_latex_()
 
     @pytest.mark.parametrize("params,result", [
-        ({"OBLIQUITY": "23;51,20"}, "02;01,12,38"),
-        ({"OBLIQUITY": "0"}, "0"),
+        ({"OBLIQUITY": "23;51,20"}, ("02;01,12,38", "23;51,20,22")),
+        ({"OBLIQUITY": "0"}, ("0", "0")),
     ])
     def test_declination(self, params, result):
         data: str = self.get_nb("declination", params).cells[-1].outputs[0].data["text/html"]
         lines = data.split("<tr>")
-        line = [li for li in lines if "<td>05 ;" in li][0]
-        assert repr(Sexagesimal(result)).strip() in line
+        line5 = [li for li in lines if "<td>05 ;" in li][0]
+        line90 = [li for li in lines if "<td>01,30 ;" in li][0]
+        assert repr(Sexagesimal(result[0])).strip() in line5
+        assert repr(Sexagesimal(result[1])).strip() in line90
