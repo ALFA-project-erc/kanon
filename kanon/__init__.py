5f3c8545ca654eb9a958736180c950a65393b1c1
Léni Gauffier <lenigauffier@gmail.com>
2021-08-26 12:13:15 +0000

    [Docs] fix docs not building

--- kanon/__init__.py
+++ kanon/__init__.py
@@ -3,3 +3,8 @@
 # Packages may add whatever they like to this file, but
 # should keep this content at the top.
 # ----------------------------------------------------------------------------
+
+try:
+    from .version import version as __version__
+except ImportError:
+    __version__ = ""
