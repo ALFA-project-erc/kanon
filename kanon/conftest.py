1c8230ce916adb3f45f132722aa2c2a9ab6c1b70
Léni Gauffier <lenigauffier@gmail.com>
2022-01-17 17:48:48 +0100

    [Tests] Fix Hypothesis type registration

--- kanon/conftest.py
+++ kanon/conftest.py
@@ -1,15 +1,3 @@
-"""Configure Test Suite.
-
-This file is used to configure the behavior of pytest when using the Astropy
-test infrastructure. It needs to live inside the package in order for it to
-get picked up when running the tests inside an interpreter using
-packagename.test
-
-"""
-
-from hypothesis.strategies._internal.core import tuples
-
-
 def _hypothesis_sexagesimal_strategy():
     """We define hypothesis strategy to generate Sexagesimal values in tests"""
     from hypothesis import HealthCheck, settings
@@ -20,6 +8,7 @@ def _hypothesis_sexagesimal_strategy():
         lists,
         register_type_strategy,
         sampled_from,
+        tuples,
     )
 
     from kanon.units import Historical, Sexagesimal
@@ -46,15 +35,4 @@ def _hypothesis_sexagesimal_strategy():
     register_type_strategy(Historical, strat)
 
 
-# Uncomment the last two lines in this block to treat all DeprecationWarnings as
-# exceptions. For Astropy v2.0 or later, there are 2 additional keywords,
-# as follow (although default should work for most cases).
-# To ignore some packages that produce deprecation warnings on import
-# (in addition to 'compiler', 'scipy', 'pygments', 'ipykernel', and
-# 'setuptools'), add:
-#     modules_to_ignore_on_import=['module_1', 'module_2']
-# To ignore some specific deprecation warning messages for Python version
-# MAJOR.MINOR or later, add:
-#     warnings_to_ignore_by_pyver={(MAJOR, MINOR): ['Message to ignore']}
-# from astropy.tests.helper import enable_deprecations_as_exceptions  # noqa
-# enable_deprecations_as_exceptions()
+_hypothesis_sexagesimal_strategy()
