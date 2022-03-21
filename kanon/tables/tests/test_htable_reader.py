43016c290a7a795485605fab572b3a6947d22097
Léni <lenigauffier@gmail.com>
2022-03-21 16:48:48 +0100

    Fix IntegerAndSexagesimal sign parsing in HTable.read (#36)

--- kanon/tables/tests/test_htable_reader.py
+++ kanon/tables/tests/test_htable_reader.py
@@ -1,21 +1,19 @@
 from kanon.tables.htable_reader import read_historical, read_intsexag_array
-from kanon.units import Historical
-from kanon.units.definitions import IntegerAndSexagesimal
+from kanon.units import Historical, IntegerAndSexagesimal
 
 
 def test_read_historical():
 
-    assert read_historical(["12", "0"], 0) == Historical(1, 0, 0)
-    assert read_historical(["11", "0"], 0) == Historical(11, 0)
-    assert read_historical(["5"], 0) == Historical(5)
-    assert read_historical(["9", "10", "5"], 0) == Historical(9, 10, 5)
-    assert read_historical(["9", "10", "5"], 1) == Historical((9, 10), (5,))
-    assert read_historical(["-9", "10", "5"], 1) == -Historical((9, 10), (5,))
+    assert read_historical([12, 0], 0) == Historical(1, 0, 0)
+    assert read_historical([11, 0], 0) == Historical(11, 0)
+    assert read_historical([5], 0) == Historical(5)
+    assert read_historical([9, 10, 5], 0) == Historical(9, 10, 5)
+    assert read_historical([9, 10, 5], 1) == Historical((9, 10), (5,))
+    assert read_historical([9, 10, 5], 1, -1) == -Historical((9, 10), (5,))
 
 
 def test_read_intsexag():
 
-    assert read_intsexag_array(["1", "12", "0"], 2) == IntegerAndSexagesimal("1; 12, 0")
-    assert read_intsexag_array(["121", "12", "0"], 2) == IntegerAndSexagesimal(
-        "121; 12, 0"
-    )
+    assert read_intsexag_array([1, 12, 0], 2, -1) == IntegerAndSexagesimal("-1; 12, 0")
+    assert read_intsexag_array([0, 12, 0], 2, -1) == IntegerAndSexagesimal("-0; 12, 0")
+    assert read_intsexag_array([121, 12, 0], 2) == IntegerAndSexagesimal("121; 12, 0")
